785a54253bfac4fd24ce9d4abf2d3ff3aa4192f3
gituser26071977 <gituser26071977@gmail.com>
2026-06-09 22:11:55 -0300

    fix: adiciona import os em routes/anamneses.py

--- routes/anamneses.py
+++ routes/anamneses.py
@@ -7,6 +7,7 @@ from flask import Blueprint, request, jsonify
 from flask_jwt_extended import jwt_required, get_jwt_identity
 from models import db, Anamnese, Paciente, Profissional
 from sqlalchemy.exc import SQLAlchemyError
+import os
 import logging
 
 logger = logging.getLogger(__name__)
